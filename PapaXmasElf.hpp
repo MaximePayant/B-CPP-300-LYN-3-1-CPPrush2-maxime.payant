368a9ca42aa9588cad4015a98db864a8772a7cbf
Maxime Payant <maxime@pop-os.localdomain>
2021-01-16 10:18:51 +0100

    Push Elf interface and begining of the PapaXmasElf

---
+++ PapaXmasElf.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2021
+** CPP_D07M
+** File description:
+** PapaXmasElf.hpp
+*/
+
+#ifndef PAPAXMAS_ELF
+#define PAPAXMAS_ELF
+
+#include <iostream>
+
+#include "IElf.hpp"
+
+class PapaXmasElf :
+    public IElf
+{
+
+    private:
+
+    public:
+        PapaXmasElf();
+
+};
+
+#endif // PAPAXMAS_ELF
