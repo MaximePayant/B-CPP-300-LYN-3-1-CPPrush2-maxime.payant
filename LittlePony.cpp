0a88f9a6ea6c704f07edc24a8175f02130c6b4bc
sebastien <sebastien.soriano@epitech.eu>
2021-01-16 09:56:11 +0100

    isTaken function for PONY

---
+++ LittlePony.cpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-300-LYN-3-1-CPPrush2-prince.miyigbena
+** File description:
+** LittlePoney
+*/
+
+#include "LittlePony.hpp"
+#include <iostream>
+
+LittlePony::LittlePony()
+{
+}
+
+LittlePony::~LittlePony()
+{
+}
+
+void LittlePony::isTaken()
+{
+    std::cout << "yo man" << std::endl;
+}
---
+++ LittlePony.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-300-LYN-3-1-CPPrush2-prince.miyigbena
+** File description:
+** LittlePoney
+*/
+
+#ifndef LITTLEPONEY_HPP_
+#define LITTLEPONEY_HPP_
+#include "Toy.hpp"
+
+class LittlePony : public Toy {
+    public:
+        LittlePony();
+        ~LittlePony();
+        void isTaken();
+    protected:
+    private:
+};
+
+#endif /* !LITTLEPONEY_HPP_ */
