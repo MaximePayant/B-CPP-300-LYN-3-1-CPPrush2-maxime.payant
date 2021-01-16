550d7f47059e6ee4540faba60408c65c76f53de6
sebastien <sebastien.soriano@epitech.eu>
2021-01-16 09:33:06 +0100

    -> CLASS OBJECT <-

---
+++ Object.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-300-LYN-3-1-CPPrush2-prince.miyigbena
+** File description:
+** Object
+*/
+
+#include "Object.hpp"
+
+Object::Object()
+{
+}
+
+Object::~Object()
+{
+}
---
+++ Object.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-300-LYN-3-1-CPPrush2-prince.miyigbena
+** File description:
+** Object
+*/
+
+#ifndef OBJECT_HPP_
+#define OBJECT_HPP_
+
+class Object {
+    public:
+        Object();
+        ~Object();
+    protected:
+};
+
+#endif /* !OBJECT_HPP_ */
