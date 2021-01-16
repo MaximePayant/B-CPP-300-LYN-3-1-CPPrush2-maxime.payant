e5e318737b2de6760e7d8d979366b304cd85310f
maugan.verdier <maugan.verdier@epitech.eu>
2021-01-16 17:38:25 +0100

    Fixing wrap

--- Wrap.cpp
+++ Wrap.cpp
@@ -8,7 +8,7 @@
 #include "Wrap.hpp"
 #include "Object.hpp"
 
-Wrap::Wrap(const std::string &name) : Object(name)
+Wrap::Wrap(const std::string &name) : Object(name, "Wrap")
 {
     this->_is_gift = false;
     this->_is_open = false;
